23b93a9565da016dd8884d69440010d9dd571eec
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-08-08 18:57:20 +0200

    u

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -8,7 +8,7 @@ done
 
 # Initialize the MySQL data directory and create the MySQL tables
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
 # Start MySQL server in the background
