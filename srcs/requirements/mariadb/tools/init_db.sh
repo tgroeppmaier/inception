e01fe596be3652b4bfcfe68b58a615a5e54919ee
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-08-09 17:12:59 +0200

    mdb

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -23,40 +23,10 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
     # Stop MySQL
     mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+else
+    # If the database is already initialized, just start MySQL
+    exec mysqld --user=mysql
 fi
 
 # Start MySQL in the foreground
 exec mysqld --user=mysql
-# #!/bin/bash
-# set -e
-
-# # Wait for MySQL to be ready
-# until mysqladmin ping -h"localhost" --silent; do
-#     sleep 1
-# done
-
-# # Initialize the MySQL data directory and create the MySQL tables
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
-#     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-# fi
-
-# # Start MySQL server in the background
-# mysqld_safe &
-
-# # Wait for MySQL to be ready again
-# until mysqladmin ping -h"localhost" --silent; do
-#     sleep 1
-# done
-
-# # Run the initialization commands
-# mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-# mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-
-# # Stop the background MySQL process
-# mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-
-# # Start MySQL in the foreground
-# exec mysqld
