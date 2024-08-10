5d6cb31d5328242b14db8faee0ada4a5087cb632
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-08-10 18:17:37 +0200

    init db

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -18,7 +18,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
     mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mysql -e "FLUSH PRIVILEGES;"
 
     # Stop MySQL
