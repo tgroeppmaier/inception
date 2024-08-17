51961268ffeb244ffc6fb70c5ec63a6e57d80cea
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-08-17 09:11:43 +0200

    db connection working

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -31,37 +31,52 @@ fi
 # Start MySQL in the foreground
 exec mysqld --user=mysql --console
 
-# #!/bin/bash
-# set -e
 
-# # Check if the database is already initialized
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
-#     # Initialize the MySQL data directory
-#     mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# #!/bin/sh
 
-#     # Start MySQL server
-#     mysqld --user=mysql &
+# echo "[DB config] Configuring MariaDB..."
 
-#     # Wait for MySQL to be ready
-#     until mysqladmin ping -h"localhost" --silent; do
-#         sleep 1
-#     done
-
-#     # Run the initialization commands
-#     mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-#     mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-#     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-#     # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-#     mysql -e "FLUSH PRIVILEGES;"
-
-#     # Stop MySQL
-#     mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-#     echo "db successfully initialized"
-# else
-#     # If the database is already initialized, just start MySQL
-#     exec mysqld --user=mysql
-#     echo "db was already initialized"
+# if [ ! -d "/run/mysqld" ]; then
+#     echo "[DB config] Granting MariaDB daemon run permissions..."
+#     mkdir -p /run/mysqld
+#     chown -R mysql:mysql /run/mysqld
 # fi
 
-# # Start MySQL in the foreground
-# exec mysqld --user=mysql
+# if [ -d "/var/lib/mysql/mysql" ]
+# then
+#     echo "[DB config] MariaDB already configured."
+# else
+#     echo "[DB config] Installing MySQL Data Directory..."
+#     chown -R mysql:mysql /var/lib/mysql
+#     mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+#     echo "[DB config] MySQL Data Directory done."
+
+#     echo "[DB config] Configuring MySQL..."
+#     TMP=/tmp/.tmpfile
+
+#     echo "USE mysql;" > ${TMP}
+#     echo "FLUSH PRIVILEGES;" >> ${TMP}
+#     echo "DELETE FROM mysql.user WHERE User='';" >> ${TMP}
+#     echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
+#     echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
+#     echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
+#     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> ${TMP}
+#     echo "CREATE DATABASE ${MYSQL_DATABASE};" >> ${TMP}
+#     echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> ${TMP}
+#     echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" >> ${TMP}
+#     echo "FLUSH PRIVILEGES;" >> ${TMP}
+
+#     /usr/bin/mysqld --user=mysql --bootstrap < ${TMP}
+#     rm -f ${TMP}
+#     echo "[DB config] MySQL configuration done."
+# fi
+
+# echo "[DB config] Allowing remote connections to MariaDB"
+# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+# echo "[DB config] Starting MariaDB daemon on port 3306."
+# exec /usr/bin/mysqld --user=mysql --console
+
+
+
